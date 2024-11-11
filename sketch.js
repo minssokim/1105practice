aa2c3d9f64e1b5a3ad2418ff3116622d9fb3bb08
minssokim <minssokim6@gmail.com>
2024-11-11 23:50:15 +0900

    오류인가싶어2

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function draw() {
 function mouseClicked() {
   let mPos = createVector(mouseX, mouseY);
   let Magnet = p5.Vector.add(ball.pos, mPos);
-  ball.mult(0.1);
+  Magnet.mult(0.1);
   ball.addForce(Magnet);
  ball.applyForce(Magnet)
 }
